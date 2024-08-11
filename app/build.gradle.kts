876cec1b13358091c6cd6e314c3bc67d39e1dc04
Bryan DeGrendel <bryan@degrendel.com>
2024-08-11 19:37:00 -0400

    Bump to latest dependency versions
    Also remove direct Kafka dependencies, which are brought in through
    Common lib.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version "2.0.10"
     id("com.diffplug.spotless") version "6.25.0"
     application
 }
@@ -7,6 +7,7 @@ plugins {
 spotless {
     kotlin {
         diktat()
+        toggleOffOn()
     }
     kotlinGradle {
         diktat()
@@ -53,23 +54,18 @@ run {
 
 group = "com.sirnuke.elusivebot"
 
-val kafkaApiVersion = "3.6.1"
-
 dependencies {
-    implementation("org.slf4j:slf4j-simple:2.0.12")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
     implementation("com.uchuhimo:konf:1.1.2")
     implementation("org.eclipse.jgit:org.eclipse.jgit:6.8.0.202311291450-r")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("io.ktor:ktor-network:2.3.2")
-    implementation("org.apache.kafka:kafka-streams:$kafkaApiVersion")
-    implementation("org.apache.kafka:kafka-clients:$kafkaApiVersion")
+    implementation("io.ktor:ktor-network:2.3.12")
 
     implementation("com.sirnuke.elusivebot:elusivebot-schema:0.1.0-SNAPSHOT")
     implementation("com.sirnuke.elusivebot:elusivebot-common:0.1.0-SNAPSHOT")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-    testImplementation("org.apache.kafka:kafka-streams-test-utils:$kafkaApiVersion")
 }
 
 java {
@@ -84,4 +80,11 @@ application {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.register("printVersion") {
+    doLast {
+        @Suppress("DEBUG_PRINT")
+        println(project.version)
+    }
 }
